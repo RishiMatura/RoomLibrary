2dca708207e17bf18bba71bb8ebeb51449b5cf85
RishiMatura <rishimatura27@gmail.com>
2024-02-09 18:41:32 +0530

    created constructors, getter/setters for Expense class

--- app/src/main/java/com/example/roomdatabase/Expense.java
+++ app/src/main/java/com/example/roomdatabase/Expense.java
@@ -2,6 +2,7 @@ package com.example.roomdatabase;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "Expense")
@@ -13,4 +14,39 @@ public class Expense {
     @ColumnInfo(name = "Amount")
     private String amount;
 
+    public Expense(String title, String amount) {
+        this.title = title;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    @Ignore
+    public Expense(int id, String title, String amount) {
+        this.id = id;
+        this.title = title;
+        this.amount = amount;
+    }
 }
