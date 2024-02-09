79b9849b673c5d5de6ca717a8063db1231205ffd
RishiMatura <rishimatura27@gmail.com>
2024-02-09 20:14:02 +0530

    added all 3 components DAO, DB, Entities. Working Fine, See log

--- app/src/main/java/com/example/roomdatabase/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.roomdatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +24,28 @@ public class MainActivity extends AppCompatActivity {
         edTitle = findViewById(R.id.enterTitle);
         edAmount = findViewById(R.id.enterAmount);
         btnAdd = findViewById(R.id.addButton);
+
+        DatabaseHelper databaseHelper = DatabaseHelper.getDB(MainActivity.this);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = edTitle.getText().toString();
+                String amount = edAmount.getText().toString();
+
+
+                databaseHelper.expenseDAO().addTx(
+                        new Expense(title,amount)
+                );
+                Toast.makeText(MainActivity.this, "Added", Toast.LENGTH_SHORT).show();
+
+                ArrayList<Expense> arrExpense = (ArrayList<Expense>) databaseHelper.expenseDAO().getAllExpense();
+                for(int i = 0; i<arrExpense.size(); i++){
+                    Log.d("Data", "Title "+arrExpense.get(i).getTitle()+" Amount "+arrExpense.get(i).getAmount());
+                }
+            }
+        });
+
+
 
     }
 }
