d6825323a9085b50dea263d84c14efa47452ff12
RishiMatura <rishimatura27@gmail.com>
2024-02-10 23:56:57 +0530

    added Adapter to Expense Class

---
+++ app/src/main/java/com/example/roomdatabase/RecyclerViewFiles/RecyclerExpenseAdapter.java
@@ -0,0 +1,46 @@
+package com.example.roomdatabase.RecyclerViewFiles;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.roomdatabase.R;
+
+import java.util.zip.Inflater;
+
+public class RecyclerExpenseAdapter extends RecyclerView.Adapter<RecyclerExpenseAdapter.ViewHolder> {
+
+    Context context;
+
+    public RecyclerExpenseAdapter(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater.from(context).inflate(R.layout.rows, parent, false);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
